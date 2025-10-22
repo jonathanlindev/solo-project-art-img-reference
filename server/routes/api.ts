7c6e98ed5424d876fc8597674adeca085001961e
Jonathan Lin <jonathanlindev@gmail.com>
2025-10-21 21:07:03 -0700

    Finished: featured, categories, and search front and back end points with routing

--- server/routes/api.ts
+++ server/routes/api.ts
@@ -5,16 +5,13 @@ import apiController from '../controllers/unsplashAPIController.ts';
 const router = express.Router();
 
 router.get('/', apiController.getPhotosData, (req, res) => {
-  // console.log(res.locals.characters);
   res.status(200).json(JSON.stringify(res.locals.photosData));
 });
 router.get('/category', apiController.getPhotosCategoryData, (req, res) => {
-  // console.log(res.locals.characters);
   res.status(200).json(JSON.stringify(res.locals.photosData));
 });
 
 router.get('/search', apiController.searchPhotosData, (req, res) => {
-  // console.log(res.locals.characters);
   res.status(200).json(JSON.stringify(res.locals.photosData));
 });
 
