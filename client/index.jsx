218c46f3a7c6620e0f2e420dfc300ad650bc4e0c
Jonathan Lin <jonathanlindev@gmail.com>
2025-10-25 12:57:23 -0700

    refactored css to fonts

--- client/index.jsx
+++ client/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './stylesheets/styles.css';
 
 import App from './App.jsx';
 
